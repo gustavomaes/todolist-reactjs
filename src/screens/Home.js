5301dc1f32013d1bd5a53d06b99a93464bacfc69
Gustavo Maes <gustavomaescd@gmail.com>
2018-08-01 23:39:00 -0300

    header

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,16 @@
 import React from 'react'
+
 import NewTask from '../components/NewTask';
+import Header from '../components/Header';
 import Table from '../components/Table';
 
 const Home = () =>
+    <div className="container">
+        <Header />
         <div className="content">
             <NewTask />
             <Table />
-
         </div>
+    </div>
 
 export default Home
-
-/*
-
-*/
